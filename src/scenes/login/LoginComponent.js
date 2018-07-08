b735cda19a7a906aafd0cc5059982658ee23f7fc
jeffersonaugusto140 <jefferson.augusto140@gmail.com>
2018-07-08 19:30:33 -0300

    Ajuste layout tela de login

--- src/scenes/login/LoginComponent.js
+++ src/scenes/login/LoginComponent.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    TextInput,
+    Button,
+    TouchableOpacity,
+    StyleSheet,
+    ImageBackground
+} from 'react-native';
 import { Actions } from 'react-native-router-flux';
+
+const bg = require('./../../imgs/bg.png');
 
 const styles = StyleSheet.create({
     title: {
@@ -15,33 +25,42 @@ const styles = StyleSheet.create({
 });
 
 export default ({ theme, user }) => (
-    <View style={{ flex: 1, padding: 10 }}>
-        <View style={styles.title}>
-            <Text style={{ fontSize: 25 }}>RN Messanger</Text>
-        </View>
-        <View style={{ flex: 2 }}>
-            <TextInput
-                style={theme.input} value={user.email} placeholder='E-mail'
-                onChangeText={(text) => user.setEmail(text)}
-                keyboardType="email-address"
-            />
-            <TextInput
-                style={theme.input} value={user.password} placeholder='Senha'
-                onChangeText={(text) => user.setPassword(text)}
-                secureTextEntry
-            />
-            <TouchableOpacity
-                onPress={() => Actions.sceneCadastro()}
-                style={styles.msgCadastro}
-                underlayColor='white'
-            >
-                <Text style={{ fontSize: 16 }}>
-                    Ainda não tem cadastro? Cadastre-se.
+    <ImageBackground style={{ flex: 1, width: null }} source={bg}>
+        <View style={{ flex: 1, padding: 10 }}>
+            <View style={styles.title}>
+                <Text style={{ fontSize: 25, color: '#fff' }}>RN Messanger</Text>
+            </View>
+            <View style={{ flex: 2 }}>
+                <TextInput
+                    value={user.email} 
+                    onChangeText={(text) => user.setEmail(text)}
+                    style={{ ...theme.input, color: '#fff' }} 
+                    placeholder='E-mail'
+                    keyboardType="email-address"
+                    placeholderTextColor="#fff"
+                />
+                <TextInput
+                    placeholder='Senha'
+                    onChangeText={(text) => user.setPassword(text)}
+                    style={{ ...theme.input, color: '#fff' }} 
+                    value={user.password} 
+                    secureTextEntry
+                    placeholderTextColor="#fff"
+                />
+                <TouchableOpacity
+                    onPress={() => Actions.sceneCadastro()}
+                    style={styles.msgCadastro}
+                    underlayColor='white'
+                >
+                    <Text style={{ fontSize: 16, color: '#fff' }}>
+                        Ainda não tem cadastro? Cadastre-se.
                 </Text>
-            </TouchableOpacity>
+                </TouchableOpacity>
+            </View>
+            <View style={{ flex: 2 }}>
+                <Button title="Acessar" color={theme.buttonColor} onPress={() => false} />
+            </View>
         </View>
-        <View style={{ flex: 2 }}>
-            <Button title="Acessar" color={theme.buttonColor} onPress={() => false} />
-        </View>
-    </View>
+    </ImageBackground>
+
 );
